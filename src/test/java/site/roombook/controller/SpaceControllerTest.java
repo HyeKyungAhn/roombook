7cbefe6ed30295a828e668b96f17bf5cd71746fe
HyeKyungAhn <hai1021@hanmail.net>
2024-09-06 00:23:33 +0900

    test: test 엔트 포인트 변경

--- src/test/java/site/roombook/controller/SpaceControllerTest.java
+++ src/test/java/site/roombook/controller/SpaceControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -38,13 +39,14 @@ class SpaceControllerTest {
 
     @Test
     @Transactional
+    @WithMockUser(roles = "EMPL_ADMIN")
     @DisplayName("space값이 다 안들어오는 경우")
     void invalidSpaceTest() throws Exception {
         String spaceFacility = "[{\"rescNo\":1,\"value\":\"wifi\",\"color\":\"hsl(282,48%,71%)\",\"style\":\"--tag-bg:hsl(282,48%,71%)\",\"__isValid\":true,\"__tagId\":\"9cca6978-ca3c-478b-a51f-b5271ce21a94\"}]";
 
         when(spaceDao.update(any())).thenReturn(1);
 
-         mockMvc.perform(post("/admin-spaces/123123")
+         mockMvc.perform(post("/api/admin/spaces/123123")
                 .param("space", "{'spaceNm': '이름'}")
                 .param("spaceFacility", spaceFacility)
                 .contentType("mulitpart/form-data"))
