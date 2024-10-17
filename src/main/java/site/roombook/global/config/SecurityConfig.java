c2f5174f479321e8b91e86c14146075f35705e03
HyeKyungAhn <hai1021@hanmail.net>
2024-10-17 15:56:18 +0900

    chore: 라이브러리 추가, SecurityConfig 경로 추가
    - spring-security-taglibs, pom.xml에 추가

--- src/main/java/site/roombook/global/config/SecurityConfig.java
+++ src/main/java/site/roombook/global/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
                 .addFilterAfter(jsonUsernamePasswordSignInFilter(), JWTAuthenticationFilter.class)
                 .authorizeHttpRequests(authorize -> authorize
                         .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll()
-                        .requestMatchers("/js/**", "/css/**", "/img/**").permitAll()
+                        .requestMatchers("/js/**", "/css/**", "/img/**", "/uploads/thumbnails/*", "/font/**").permitAll()
                         .requestMatchers("/favicon/**").permitAll()
                         .requestMatchers("/", "/signin/**", "/api/signin/**", "/signup/**", "/api/signup/**", "/password-reset/**", "/id-inquiry/**", "/error/**", "/invalid-access/**", "/not-found/**", "/api/empls/*/dupcheck").permitAll()
                         .requestMatchers( "/spaces/**", "/book/spaces/**", "/api/spaces/**", "/api/book/**", "/api/mybook/**", "/mybook/**").access(userAuthorityAuthorizationManager())
